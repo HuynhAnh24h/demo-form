44e7bd10e8119c583e54c97c8532b993a7484a5a
HuynhAnh24h <anhdev24h@gmail.com>
2025-07-17 13:45:54 +0700

    Done

--- src/components/common/ThanhYou.jsx
+++ src/components/common/ThanhYou.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-
+import logo from "@/assets/logo.png"
 const ThanhYou = () => {
   return (
     <div className='max-w-md mf:max-w-xl min-h-[500px] flex flex-col items-center justify-center shadow-sm gap-3 mx-3 px-3'>
-        <h1 className='text-lg font-bold text-orange-400 text-center'>CHANCHAN CÁM ƠN BẠN RẤT NHIỀU 🧡</h1>
-        <p className='text-sm text-[#60230D] font-medium bg-orange-50 rounded p-3 text-center'>ChanChan thỉnh thoảng sẽ gửi tặng quà cho khách hàng may mắn hoặc thân thiết nhất. Nếu bạn muốn nhận quà bất ngờ từ tụi mình, để lại thông tin nha 😍
-        </p>
+     <div className="w-[150px] h-[150px]">
+       <img src={logo} alt="Logo ChanChan" className="w-full h-full mb-4" />
+     </div>
+        <h1 className='text-lg font-bold text-orange-400 text-center'>CHANCHAN CẢM ƠN BẠN RẤT NHIỀU 🧡</h1>
     </div>
   )
 }
