765b3bc4187a658ee8f0cd89379c740fa213778a
HuynhAnh24h <anhdev24h@gmail.com>
2025-07-18 08:44:18 +0700

    Done Clear LocalStorage and update constant survay json

--- src/components/common/SurvayForm.jsx
+++ src/components/common/SurvayForm.jsx
@@ -17,7 +17,7 @@ const schema = useSurveySchema()
 
 function SurveyForm() {
     const [step, setStep] = useState(0)
-    const [thank,setThank] = useState(false)
+    const [thank, setThank] = useState(false)
     const currentStep = survay[step]
     const allKeys = survay.flatMap(step => step.questions.map(q => q.id.toString()))
     const saved = localStorage.getItem("surveyAnswers")
@@ -42,18 +42,25 @@ function SurveyForm() {
 
     const answers = watch()
 
+    const [isSubmitted, setIsSubmitted] = useState(false)
+
     useEffect(() => {
-        localStorage.setItem("surveyAnswers", JSON.stringify(answers))
-    }, [answers])
+        if (!isSubmitted) {
+            localStorage.setItem("surveyAnswers", JSON.stringify(answers))
+        }
+    }, [answers, isSubmitted])
 
     const onSubmit = (data) => {
+        setIsSubmitted(true)
         const formatted = Object.entries(data).map(([id, answer]) => ({
             questionId: id,
             answer,
         }))
         console.log("Dữ liệu gửi lên server:", formatted)
         toast.success("Đã gửi khảo sát thành công!")
-        localStorage.clear()
+
+        localStorage.removeItem("surveyAnswers") // ✅ Xóa đúng key cần thiết
+        reset({})
         setThank(true)
     }
 
@@ -67,88 +74,88 @@ function SurveyForm() {
     }
 
     return (
-       <>
-       {
-        thank ? <ThanhYou/>:(
-            <div className="max-w-md md:min-w-xl shadow-xl flex flex-col justify-center items-center mx-3 my-3 bg-white p-6 rounded-lg">
-            <img src={logo} alt="Logo ChanChan" className="w-28 h-28 mb-4" />
+        <>
+            {
+                thank ? <ThanhYou /> : (
+                    <div className="max-w-md md:min-w-xl shadow-xl flex flex-col justify-center items-center mx-3 my-3 bg-white p-6 rounded-lg">
+                        <img src={logo} alt="Logo ChanChan" className="w-28 h-28 mb-4" />
 
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 w-full">
-                <h2 className="text-lg font-bold bg-[#FF6600] text-white text-center py-3 rounded-md shadow">
-                    {currentStep.categoryName}
-                </h2>
-                {currentStep.text && (
-                    <p className="text-sm text-[#60230D] font-medium bg-orange-50 rounded p-3 text-center">
-                        {currentStep.text}
-                    </p>
-                )}
-                {currentStep.questions.map(q => (
-                    <div key={q.id} className="space-y-2">
-                        <Label className="text-orange-500 font-semibold block">{q.title}</Label>
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 w-full">
+                            <h2 className="text-lg font-bold bg-[#FF6600] text-white text-center py-3 rounded-md shadow">
+                                {currentStep.categoryName}
+                            </h2>
+                            {currentStep.text && (
+                                <p className="text-sm text-[#60230D] font-medium bg-orange-50 rounded p-3 text-center">
+                                    {currentStep.text}
+                                </p>
+                            )}
+                            {currentStep.questions.map(q => (
+                                <div key={q.id} className="space-y-2">
+                                    <Label className="text-orange-500 font-semibold block">{q.title}</Label>
 
-                        {q.typeOfQuestion === 1 && Array.isArray(q.answers) ? (
-                            <RadioGroup
-                                value={answers[q.id] ?? ""}
-                                onValueChange={val =>
-                                    setValue(q.id.toString(), val ?? "", {
-                                        shouldValidate: true,
-                                        shouldDirty: true,
-                                    })
-                                }
-                                className={q.answers.length == 3 ? "grid md:grid-cols-3 grid-cols-3 gap-1" : "space-y-1"}
-                            >
-                                {q.answers.map((a, idx) => {
-                                    const inputId = `${q.id}-${idx}`
-                                    return (
-                                        <Label
-                                            key={inputId}
-                                            htmlFor={inputId}
-                                            className="flex items-center gap-2 rounded-md p-1 text-[10px] font-medium text-gray-700 cursor-pointer
+                                    {q.typeOfQuestion === 1 && Array.isArray(q.answers) ? (
+                                        <RadioGroup
+                                            value={answers[q.id] ?? ""}
+                                            onValueChange={val =>
+                                                setValue(q.id.toString(), val ?? "", {
+                                                    shouldValidate: true,
+                                                    shouldDirty: true,
+                                                })
+                                            }
+                                            className={q.answers.length == 3 ? "grid md:grid-cols-3 grid-cols-3 gap-1" : "space-y-1"}
+                                        >
+                                            {q.answers.map((a, idx) => {
+                                                const inputId = `${q.id}-${idx}`
+                                                return (
+                                                    <Label
+                                                        key={inputId}
+                                                        htmlFor={inputId}
+                                                        className="flex items-center gap-2 rounded-md p-1 text-[10px] font-medium text-gray-700 cursor-pointer
                                         hover:bg-orange-50 hover:border-orange-300
                                         transition-colors duration-200"
-                                        >
-                                            <RadioGroupItem value={a.answer} id={inputId} />
-                                            {a.answer}
-                                        </Label>
-                                    )
-                                })}
-                            </RadioGroup>
+                                                    >
+                                                        <RadioGroupItem value={a.answer} id={inputId} />
+                                                        {a.answer}
+                                                    </Label>
+                                                )
+                                            })}
+                                        </RadioGroup>
 
 
-                        ) : (
-                            <Input
-                                {...register(q.id.toString())}
-                                className="w-full border border-gray-300 p-2 rounded text-sm font-medium outline-none focus:border-orange-500 focus:bg-orange-50 focus:text-orange-600"
-                            />
-                        )}
+                                    ) : (
+                                        <Input
+                                            {...register(q.id.toString())}
+                                            className="w-full border border-gray-300 p-2 rounded text-sm font-medium outline-none focus:border-orange-500 focus:bg-orange-50 focus:text-orange-600"
+                                        />
+                                    )}
 
-                        {errors[q.id]?.message && (
-                            <p className="text-sm text-red-500">{errors[q.id]?.message}</p>
-                        )}
+                                    {errors[q.id]?.message && (
+                                        <p className="text-sm text-red-500">{errors[q.id]?.message}</p>
+                                    )}
+                                </div>
+                            ))}
+
+                            <div className="flex justify-between pt-4">
+                                {step > 0 && (
+                                    <Button type="button" onClick={() => setStep(step - 1)} className="bg-orange-500 hover:bg-orange-600 text-white">
+                                        Quay lại
+                                    </Button>
+                                )}
+                                {step < survay.length - 1 ? (
+                                    <Button type="button" onClick={handleNextStep} className="bg-orange-500 hover:bg-orange-600 text-white">
+                                        Tiếp theo
+                                    </Button>
+                                ) : (
+                                    <Button type="submit" className="bg-green-500 hover:bg-green-600 text-white">
+                                        Gửi khảo sát
+                                    </Button>
+                                )}
+                            </div>
+                        </form>
                     </div>
-                ))}
-
-                <div className="flex justify-between pt-4">
-                    {step > 0 && (
-                        <Button type="button" onClick={() => setStep(step - 1)} className="bg-orange-500 hover:bg-orange-600 text-white">
-                            Quay lại
-                        </Button>
-                    )}
-                    {step < survay.length - 1 ? (
-                        <Button type="button" onClick={handleNextStep} className="bg-orange-500 hover:bg-orange-600 text-white">
-                            Tiếp theo
-                        </Button>
-                    ) : (
-                        <Button type="submit" className="bg-green-500 hover:bg-green-600 text-white">
-                            Gửi khảo sát
-                        </Button>
-                    )}
-                </div>
-            </form>
-        </div>
-        )
-       }
-       </>
+                )
+            }
+        </>
     )
 }
 
